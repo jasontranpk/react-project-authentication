bdc48ff57592bf1f597359f3bfdb1bd1e7c8a9f8
Jason Tran <jasontran.pk@gmail.com>
2022-07-25 13:58:21 +0700

    add authentication functions

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,107 @@
-import { useState } from 'react';
+import { useRef, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import AuthContext from '../../store/auth-context';
 import classes from './AuthForm.module.css';
-
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+	const history = useHistory();
+	const [isLogin, setIsLogin] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+	const authCtx = useContext(AuthContext);
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	};
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+		setIsLoading(true);
+		let url;
+		if (isLogin) {
+			url =
+				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBwD7lpJ3LDCLtmSeFf5LLVAooNbahAdx4';
+		} else {
+			url =
+				'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBwD7lpJ3LDCLtmSeFf5LLVAooNbahAdx4';
+		}
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				email: enteredEmail,
+				password: enteredPassword,
+				returnSecureToken: true,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => {
+				setIsLoading(false);
+				if (res.ok) {
+					return res.json();
+				} else {
+					return res.json().then((data) => {
+						console.log(data);
+						let errorMessage = 'Authentication failed';
+						if (data && data.error && data.error.message)
+							errorMessage = data.error.message;
+						throw new Error(errorMessage);
+					});
+				}
+			})
+			.then((data) => {
+				const expirationTime = new Date(
+					new Date().getTime() + +data.expiresIn * 1000
+				);
+				authCtx.login(data.idToken, expirationTime.toISOString());
+				history.replace('/');
+			})
+			.catch((error) => alert(error));
+	};
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor='email'>Your Email</label>
+					<input
+						type='email'
+						id='email'
+						ref={emailInputRef}
+						required
+					/>
+				</div>
+				<div className={classes.control}>
+					<label htmlFor='password'>Your Password</label>
+					<input
+						type='password'
+						ref={passwordInputRef}
+						id='password'
+						required
+					/>
+				</div>
+				<div className={classes.actions}>
+					{!isLoading && (
+						<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					)}
+					{isLoading && <p> Loading... </p>}
+					<button
+						type='button'
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+						{isLogin
+							? 'Create new account'
+							: 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default AuthForm;
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
+import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const history = useHistory();
+	const authCtx = useContext(AuthContext);
+	const newPassInputRef = useRef();
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredPass = newPassInputRef.current.value;
+		fetch(
+			'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBwD7lpJ3LDCLtmSeFf5LLVAooNbahAdx4',
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredPass,
+					returnSecureToken: false,
+				}),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then((response) => {
+			console.log(response);
+			history.replace('/');
+		});
+	};
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor='new-password'>New Password</label>
+				<input
+					type='password'
+					ref={newPassInputRef}
+					id='new-password'
+					minLength='7'
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
